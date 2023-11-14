66f5b93b69f86cabd2c33568c0eb7d0987bae54f
RephlexZero <rephlexzero@protonmail.com>
2023-11-14 17:05:17 +0000

    Working state

--- keyboards/rephlex/moonboard/mcuconf.h
+++ keyboards/rephlex/moonboard/mcuconf.h
@@ -6,3 +6,19 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 
 #undef STM32_ADC_USE_ADC1
 #define STM32_ADC_USE_ADC1 TRUE
+
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 TRUE
+
+#undef STM32_PWM_USE_ADVANCED
+#define STM32_PWM_USE_ADVANCED TRUE
+
+#undef STM32_PWM_USE_TIM8
+#define STM32_PWM_USE_TIM8 TRUE
+
+#undef STM32_DAC_USE_DAC1_CH1
+#define STM32_DAC_USE_DAC1_CH1              TRUE
+#undef STM32_DAC_USE_DAC1_CH2
+#define STM32_DAC_USE_DAC1_CH2              TRUE
+#undef STM32_GPT_USE_TIM6
+#define STM32_GPT_USE_TIM6                  TRUE
--- keyboards/rephlex/moonboard/rules.mk
+++ keyboards/rephlex/moonboard/rules.mk
@@ -1,6 +1,11 @@
 QUANTUM_LIB_SRC += analog.c
 SRC += matrix.c lut.c scanfunctions.c multiplexer.c
 
+MCU_LDSCRIPT = STM32F303xE
+
 CUSTOM_MATRIX = lite
 
+OPT = 2
+
 ENCODER_MAP_ENABLE = yes
+AUDIO_DRIVER = dac_additive
