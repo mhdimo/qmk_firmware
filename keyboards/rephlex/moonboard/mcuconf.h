e19673b8c5d380ecba63156d6cc16856decea84b
certainly1182 <fawcettjames63@gmail.com>
2024-03-14 20:36:20 +0000

    pll values

--- keyboards/rephlex/moonboard/mcuconf.h
+++ keyboards/rephlex/moonboard/mcuconf.h
@@ -5,36 +5,37 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 #include_next <mcuconf.h>
 
 #undef STM32_ADC_USE_ADC1
-#define STM32_ADC_USE_ADC1 TRUE
+#define STM32_ADC_USE_ADC1          TRUE
 
 #undef STM32_ADC_USE_ADC2
-#define STM32_ADC_USE_ADC2 TRUE
+#define STM32_ADC_USE_ADC2          TRUE
 
 #undef STM32_ADC_USE_ADC3
-#define STM32_ADC_USE_ADC3 TRUE
+#define STM32_ADC_USE_ADC3          TRUE
 
 #undef STM32_ADC_USE_ADC4
-#define STM32_ADC_USE_ADC4 TRUE
+#define STM32_ADC_USE_ADC4          TRUE
 
 #undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1 TRUE
+#define STM32_I2C_USE_I2C1          TRUE
 
 #undef STM32_PWM_USE_ADVANCED
-#define STM32_PWM_USE_ADVANCED TRUE
+#define STM32_PWM_USE_ADVANCED      TRUE
 
 #undef STM32_PWM_USE_TIM1
-#define STM32_PWM_USE_TIM1 TRUE
+#define STM32_PWM_USE_TIM1          TRUE
 
 #undef STM32_DAC_USE_DAC1_CH1
-#define STM32_DAC_USE_DAC1_CH1              TRUE
+#define STM32_DAC_USE_DAC1_CH1      TRUE
 #undef STM32_DAC_USE_DAC1_CH2
-#define STM32_DAC_USE_DAC1_CH2              TRUE
+#define STM32_DAC_USE_DAC1_CH2      TRUE
 #undef STM32_GPT_USE_TIM6
-#define STM32_GPT_USE_TIM6                  TRUE*/
+#define STM32_GPT_USE_TIM6          TRUE
 
+#define STM32_HSE_ENABLED           TRUE
 #undef  STM32_PREDIV_VALUE
-#define STM32_PREDIV_VALUE                  1
+#define STM32_PREDIV_VALUE          2
 #undef  STM32_PLLMUL_VALUE
-#define STM32_PLLMUL_VALUE                  3
+#define STM32_PLLMUL_VALUE          9
 #undef  STM32_USBPRE
-#define STM32_USBPRE                        STM32_USBPRE_DIV1
+#define STM32_USBPRE                STM32_USBPRE_DIV1P5
--- keyboards/rephlex/moonboard/rules.mk
+++ keyboards/rephlex/moonboard/rules.mk
@@ -1,7 +1,7 @@
 QUANTUM_LIB_SRC += analog.c
 SRC += matrix.c lut.c scanfunctions.c multiplexer.c
 
-MCU_LDSCRIPT = STM32F303xB
+#MCU_LDSCRIPT = STM32F303xB
 
 CUSTOM_MATRIX = lite
 
